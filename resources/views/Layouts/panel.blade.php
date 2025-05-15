001830834ac7f40adb3d70d6d222d096c6c32698
mamad1993 <mohammad.saffarian@yahoo.com>
2025-05-15 05:20:49 +0330

    test12

--- resources/views/Layouts/panel.blade.php
+++ resources/views/Layouts/panel.blade.php
@@ -226,6 +226,7 @@ hello from outsider
 <div class="card">
     how are u
     <div class="card-header">
+        card header changes
         <h5><i class="fas fa-table me-2"></i>Expenses Table</h5>
         <div class="card-tools">
             <span class="badge bg-primary">Updated: Today</span>
